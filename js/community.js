1e5dbd6dbce8350700711dc2e304c021150ea029
heeheepark <pjh9641@gmail.com>
2023-05-25 22:25:51 +0900

    community json 데이터 출력

---
+++ js/community.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", function (event) {
+  const communityList = this.document.querySelector(".community-list");
+  console.log(communityList);
+
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function (event) {
+    let req = event.target;
+    if (req.readyState === XMLHttpRequest.DONE) {
+      let data = JSON.parse(req.response);
+      console.log(data);
+      makeCommunityHtml(data);
+    }
+  });
+  xhr.open("GET", "data/communitydata.json");
+  xhr.send();
+
+  let jsonData;
+
+  function makeCommunityHtml(_data) {
+    let html = ``;
+    jsonData = _data.notice.list;
+    let obj;
+    for (let i = 0; i < jsonData.length; i++) {
+      obj = jsonData[i];
+      let temp = `
+        <li>
+          <a href="${obj.link}">
+            <div class="list-top">
+              <span class="list-date">${obj.date}</span>
+              <span class="list-department">${obj.department}</span>
+            </div>
+            <div class="list-mid">
+              <p class="list-title">${obj.title}</p>
+            </div>
+            <div class="list-bottom">
+              <p class="list-contents">${obj.contents}</p>
+            </div>
+          </a>
+        </li>
+        `;
+      console.log(temp);
+      html += temp;
+    }
+    communityList.innerHTML = html;
+  }
+});
---
+++ js/service.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function (event) {
+  const serviceMenuBottom = this.document.querySelector(".service-menu-bottom");
+  console.log(serviceMenuBottom);
+
+  
+});
